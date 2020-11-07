f1c12270ea36e10219e08f7daeaf592128d1cfc3
Mirwes <mirwes210@gmail.com>
2020-11-07 10:55:31 -0800

    completed the web-sprint-challenge-advanced

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,29 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
-import Login from "./components/Login";
-import "./styles.scss";
+import React from 'react'
+import {Route, Switch} from 'react-router-dom'
+import PrivateRoute from './routes/PrivateRoute'
+import Login from './components/Login'
+import Bubbles from './components/BubblePage'
+import Colors from './components/Colors'
+import Navigation from './components/Nav'
+import AddColor from './components/AddColor'
+// import './styles/styles.scss'
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
-      </div>
-    </Router>
-  );
+    <>
+      <Navigation />
+
+      <Switch>
+        <PrivateRoute path='/colors' component={Colors} />
+        <PrivateRoute path='/addcolor' component={AddColor} />
+        <PrivateRoute path='/bubbles' component={Bubbles} />
+
+        <Route exact path='/' component={Login} />
+        {/* <Route exact path='*' component={<h1>404</h1>} /> */}
+      </Switch>
+    </>
+  )
 }
 
 export default App;
+
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,82 @@
-import React from "react";
+import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Logo from '../assets/images';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import {useDispatch} from 'react-redux'
+import {getLoginToken} from '../redux/actions/authActions'
+import {useHistory} from 'react-router-dom'
+import '../styles/login.scss'
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+function Login(props) {
+  // Inital Form State
+  const initialValue = {
+    email: 'bruno@aol.com',
+    password: 'password',
+  }
+
+  // Local State
+  const [value, setValue] = React.useState(initialValue)
+  const dispatch = useDispatch()
+  let history = useHistory()
+  // Handle Input Changes
+  const handleChanges = (e) => {
+    setValue({...value, [e.target.name]: e.target.value})
+  }
+
+  //HandleSubmit
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch(getLoginToken(value, history))
+  }
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+    <div className='login'>
+      <div className='wrapper'>
+        <Row>
+          <Col className='left-login'></Col>
+          <Col>
+            <div className='right-login'>
+              <Form onSubmit={handleSubmit} className='login-form'>
+                <img src={Logo} alt='logo' className='login-logo' />
 
-export default Login;
+                <Form.Group controlId='formBasicEmail'>
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    type='email'
+                    value={value.email}
+                    placeholder='Enter email'
+                    onChange={handleChanges}
+                    name='email'
+                  />
+                  <Form.Text className='text-muted'>
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId='formBasicPassword'>
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type='password'
+                    placeholder='Password'
+                    value={value.passowrd}
+                    onChange={handleChanges}
+                    name='password'
+                  />
+                </Form.Group>
+
+                <Button variant='primary' type='submit'>
+                  Submit
+                </Button>
+              </Form>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  )
+}
+
+export default Login
